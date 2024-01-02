9eb36d52c895a59b2a0cb66a385159eb57f8425d
Jeff Gordon <jgordon005@gmail.com>
2024-01-02 15:49:00 -0700

    Updated copyright year
    It's 2024

--- web/old_common/js/score/tts/deuce.js
+++ web/old_common/js/score/tts/deuce.js
@@ -1,7 +1,7 @@
 /******************************************************************************
  * The MIT License (MIT)
  *
- * Copyright (c) 2013 blackchip.org
+ * Copyright (c) 2013-2024 blackchip.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
